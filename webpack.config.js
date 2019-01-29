c5eb736cbf4156b79db9254d978dc29f399a28c8
su-kun1899 <higedrum.coz@gmail.com>
2019-01-29 20:03:28 +0900

    clean-webpack-plugin を使う

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
   entry: { main: './src/index.js' },
   plugins: [
+    new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: './src/html/index.html',
