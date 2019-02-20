d71c77d05cf5ea0840332606b1a853bbb24e8a53
su-kun1899 <higedrum.coz@gmail.com>
2019-02-20 13:34:18 +0900

    TodoListViewを作成する

---
+++ src/view/TodoListView.js
@@ -0,0 +1,22 @@
+import { element } from './html-util';
+import { TodoItemView } from './TodoItemView';
+
+export class TodoListView {
+  /**
+   * `todoItems`に対応するTodoリストのHTML要素を作成して返す
+   * @param {TodoItemModel[]} todoItems TodoItemModelの配列
+   * @param {function({id: String, completed: boolean})} onUpdateTodo チェックボックスの更新イベントリスナー
+   * @param {function({id: String})} onDeleteTodo 削除ボタンのクリックイベントリスナー
+   * @returns {Element} TodoItemModelの配列に対応したリストのHTML要素
+   */
+  createElement(todoItems, { onUpdateTodo, onDeleteTodo }) {
+    const todoListElement = element`<ul />`;
+    todoItems.forEach(todoItem => {
+      const todoItemView = new TodoItemView();
+      const todoItemElement = todoItemView.createElement(todoItem, onUpdateTodo, onDeleteTodo);
+
+      todoListElement.appendChild(todoItemElement);
+      return todoListElement;
+    });
+  }
+}
